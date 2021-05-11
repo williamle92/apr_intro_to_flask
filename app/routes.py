c89d90179b1efea4f4f3d450da98a49e58c685a5
Brian <bstanton773@gmail.com>
2021-05-11 11:49:19 -0500

    Add UserInfoForm and add to route/html

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+from app.forms import UserInfoForm
 
 @app.route('/')
 def index():
@@ -14,7 +15,14 @@ def index():
     return render_template('index.html', **context)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     title = 'REGISTER'
-    return render_template('register.html', title=title)
+    form = UserInfoForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        print(username, email, password)
+
+    return render_template('register.html', title=title, form=form)
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 class Config():
-    FLASK_APP=os.environ.get('FLASK_APP')
-    FLASK_ENV=os.environ.get('FLASK_ENV')
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
