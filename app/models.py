7968f6e29fee3ce81b3825c44bb14684889501cf
Brian <bstanton773@gmail.com>
2021-05-11 14:31:19 -0500

    Add db and user table to database

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app import db
+from werkzeug.security import generate_password_hash
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
