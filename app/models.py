5501ce5a05b3a4756476fa22271dd05ae78c149c
Brian <bstanton773@gmail.com>
2021-05-11 15:52:20 -0500

    Add post table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from werkzeug.security import generate_password_hash
+from datetime import datetime
 
 
 class User(db.Model):
@@ -7,6 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, username, email, password):
         self.username = username
@@ -15,3 +17,19 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User | {self.username}>'
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(300))
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, title, body, user_id):
+        self.title = title
+        self.body = body
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f'<Post | {self.title}>'
