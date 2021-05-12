741a1b48692d29eaf0b2526a6597d1e97ea81d9b
Brian <bstanton773@gmail.com>
2021-05-12 11:31:16 -0500

    Add flask-login

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms import validators
 from wtforms.validators import DataRequired, EqualTo, Email
 
 class UserInfoForm(FlaskForm):
@@ -7,4 +8,10 @@ class UserInfoForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField()
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app, db
 from flask import render_template, request, redirect, url_for, flash
-from app.forms import UserInfoForm
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import check_password_hash
+from app.forms import UserInfoForm, LoginForm
 from app.models import User, Post
 
 @app.route('/')
@@ -40,3 +42,18 @@ def register():
 
 
     return render_template('register.html', title=title, form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    title = 'LOGIN'
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+        if user is None or not check_password_hash(user.password, password):
+            flash('Incorrect Username/Password. Please try again.', 'danger')
+            return redirect(url_for('login'))
+    return render_template('login.html', title=title, form=form)
