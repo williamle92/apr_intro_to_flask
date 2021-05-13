c14204579fce2b1a9e4a3c3eb899abbdec44844b
Brian <bstanton773@gmail.com>
2021-05-13 11:02:51 -0500

    Added first blueprint

--- app/forms.py
+++ app/forms.py
@@ -3,19 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms import validators
 from wtforms.validators import DataRequired, EqualTo, Email
 
-class UserInfoForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField()
 
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField()
 
 
 class PostForm(FlaskForm):
--- app/routes.py
+++ app/routes.py
@@ -15,64 +15,6 @@ def index():
     return render_template('index.html', **context)
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    title = 'REGISTER'
-    form = UserInfoForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        username = form.username.data
-        email = form.email.data
-        password = form.password.data
-        # print(username, email, password)
-        # Check if username/email already exists
-        existing_user = User.query.filter((User.username == username) | (User.email == email)).all()
-        if existing_user:
-            flash('That username or email already exists. Please try again', 'danger')
-            return redirect(url_for('register'))
-        
-        new_user = User(username, email, password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        flash(f'Thank you {username} for registering!', 'success')
-
-        msg = Message(f'Thank you, {username}', recipients=[email])
-        msg.body = f'Dear {username}, thank you so much for signing up for this super cool app. I hope you enjoy and also you look super good today!'
-        mail.send(msg)
-
-        return redirect(url_for('index'))
-
-
-    return render_template('register.html', title=title, form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    title = 'LOGIN'
-    form = LoginForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-
-        user = User.query.filter_by(username=username).first()
-        if user is None or not check_password_hash(user.password, password):
-            flash('Incorrect Username/Password. Please try again.', 'danger')
-            return redirect(url_for('login'))
-        
-        login_user(user, remember=form.remember_me.data)
-        flash('You have succesfully logged in!', 'success')
-        return redirect(url_for('index'))
-
-    return render_template('login.html', title=title, form=form)
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    flash('You have successfully logged out!', 'primary')
-    return redirect(url_for('index'))
-
-
 @app.route('/createpost', methods=['GET', 'POST'])
 @login_required
 def createpost():
